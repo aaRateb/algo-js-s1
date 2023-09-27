34ce466fba983b7fce3675b822082b78e30081ed
aaRateb <atierateb05@gmail.com>
2023-09-27 08:42:53 +0200

    Creating exo 5
    leaning to checkNames Users and compare them

---
+++ Javascript/exo 5/main.js
@@ -0,0 +1,17 @@
+var a = "Jean"
+
+var b = "Paul"
+
+var result = checkName(a, b)
+
+function checkName(userName1, userName2) {
+    if (userName1 === userName2){
+    return true
+    }
+
+    else {
+    return false
+    }
+
+} 
+    console.log(result)
---
+++ Javascript/learning 1/main.js
@@ -0,0 +1,9 @@
+let mainUser = "Gabriel"
+let secondUser = "Camille"
+let lastUser = "Pierre"
+
+function creatingUserSentence(userName) {
+    console.log()
+}
+
+creatingUserSentence (mainUser)
