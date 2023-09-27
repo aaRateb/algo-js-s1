4e9149ba4a0b254f68c2626d33dd363eba5735bd
aaRateb <atierateb05@gmail.com>
2023-09-27 12:03:42 +0200

    Learning create caracter with classes
    Learn How create caracter, and other things

---
+++ Javascript/learning 4/main.js
@@ -0,0 +1,32 @@
+class hero {
+    constructor(name, attack, pv) {
+        this.name = name
+        this.attack = attack
+        this.pv = pv
+
+    }
+
+}
+
+class monsters {
+    constructor(pv, attack) {
+        this.pv = pv
+        this.attack = attack
+
+    
+    }
+    getlife( ) {
+            console.log()
+        
+        }
+
+}
+
+let reinor = new hero ("Reinor", 12, 300)
+let gnome = new monsters (10, 1200)
+
+
+
+if (gnome.pv -= reinor.attack <=0) {
+    console.log("Le gnome est mort.")
+}
