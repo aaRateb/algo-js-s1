a3f7c6f7ea61da740169d3eb4abaec5fc570b133
aaRateb <atierateb05@gmail.com>
2023-09-27 09:37:29 +0200

    Update Learning 2
    Learning how to do board

---
+++ Javascript/learning 2/main.js
@@ -0,0 +1,27 @@
+let students = ["Camille", "Gabriel", "Etienne"]
+let numbers = [12, 13, 5, 315]
+let things = ["Bonjour", 12, 12.3, true, 12]
+let fruits = ["Pomme", "Poire", "Pèche"]
+
+console.log(fruits[1])
+
+//apprendre à faire des tableaux au dessus//
+
+
+
+/*
+
+console.log(threadAnswers.length)
+for (let i = 0; i < threadAnswers.length; i++){
+
+Permet de savoir le nombre de ligne*/
+
+
+let cart = ["Pomme", "PS5", "X"]
+cart.push ("IIM")
+console.log(cart)
+//Permet de rajouter quelqe chose dans un "panier".//
+
+cart.splice(1, 1)
+console.log(cart)
+//Permet de retirer quelque chose du panier//
