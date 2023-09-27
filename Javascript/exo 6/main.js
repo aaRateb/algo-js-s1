d2c8737b5225b7dc8b8e7104610e83cea2dc19b8
aaRateb <atierateb05@gmail.com>
2023-09-27 10:57:10 +0200

    Update Exo 6
    Correction with the profesor. And learning foreach.

--- Javascript/exo 6/main.js
+++ Javascript/exo 6/main.js
@@ -4,9 +4,15 @@ names.push("Paul")
 names.push("Arthur")
 names.push("Vincent")
 
+/*
 for (let i = 0; i < names.length; i++){
     names[i] = names[i] + " va à la pèche."
-
-console.log(names[i]) 
+    console.log(names[i]) 
 }
+/**méthode plus compliqué, en dessous plus simple */
 
+names.forEach(name => {
+    name += "va à la pèche"
+    console.log(name)
+
+})
