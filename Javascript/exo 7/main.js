24a572114bff71cdb92615687878eb3cc44e6803
aaRateb <atierateb05@gmail.com>
2023-09-27 11:52:10 +0200

    Upadte
    Create own profil and exercice, and fix latest bug, save html.

---
+++ Javascript/exo 7/main.js
@@ -0,0 +1,7 @@
+let CharacterProfil = {
+    firstname : "Rateb",
+    lastname : "Atie",
+    Age : 28,
+}
+
+console.log("Ce personnage ce nomme" + CharacterProfil.firstname + CharacterProfil.lastname + "il a", CharacterProfil.age + " et se trouve dans les champs")
