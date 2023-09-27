58a27cb6d4057b40a98c87fd2f36b021e0ba55f8
aaRateb <atierateb05@gmail.com>
2023-09-27 10:47:11 +0200

    Update Board / Exo 6
    Learn push on board.

---
+++ Javascript/exo 6/main.js
@@ -0,0 +1,12 @@
+var names = []
+names.push("Gabriel")
+names.push("Paul")
+names.push("Arthur")
+names.push("Vincent")
+
+for (let i = 0; i < names.length; i++){
+    names[i] = names[i] + " va à la pèche."
+
+console.log(names[i]) 
+}
+
