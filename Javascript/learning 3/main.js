c9f0a83769fbb80e10ba8be916732718961cf76b
aaRateb <atierateb05@gmail.com>
2023-09-27 11:07:42 +0200

    Update Leaning Character
    Learning how create dndCharacterProfil

---
+++ Javascript/learning 3/main.js
@@ -0,0 +1,11 @@
+let dndCharacterProfil = {
+    name : "Reinor",
+    level : 6,
+    class : "Paladin",
+    atk : 25,
+    def : 12.5,
+    isAlive : false,
+    inventory : ("water", "sword", "shield")
+
+}
+console.log(dndCharacterProfil)
