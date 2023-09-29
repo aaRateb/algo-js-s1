4e37d86cd291f336fd062ba92e26331d35bd594a
aaRateb <atierateb05@gmail.com>
2023-09-29 10:05:42 +0200

    Update new exo // And new folder rendu
    Correction of exo, exo 9 je n'arrive pas à le faire marcher revoir

--- Javascript/exo 9/main.js
+++ Javascript/exo 9/main.js
@@ -4,15 +4,12 @@ function countCharacter(value) {
     return value.length
 }
 
-users.forEach(user => {
-    if (countcharacter(user) < 5) {
-        console.log("C'est un prénom long de plus de 5 lettres.")
+users.forEach(user =>  {
+    if (countCharacter(user) > 5) {
+        console.log("C\'est un prénom long de plus de 5 lettres.")
     } else {
         console.log("Ce n'est pas du tout un prénom long.")
     }
 });
 
-
-
-
-
+//Le code ne marche pas, à refaire chez moi
