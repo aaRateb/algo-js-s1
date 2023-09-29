4405489de520b0b6a3784b54da97f192f6131ceb
aaRateb <atierateb05@gmail.com>
2023-09-29 09:01:50 +0200

    Update exo 8 with correction
    Correction of class and islucky

--- Javascript/exo 8/main.js
+++ Javascript/exo 8/main.js
@@ -1,14 +1,42 @@
 class pokemon {
-    constructor(name, attack, defense, hp){
-    
+    constructor(name, atk, def, hp, luck) {
         this.name = name
-        this.attack = attack
-        this.defense = defense
+        this.atk = atk
+        this.def = def
         this.hp = hp
-    }        
+        this.luck = luck
+    }
+    
+    attackPokemon(pokemon) {
+        if(this.isLucky()) {
+            let damage = this.atk - pokemon.def
+            pokemon.hp -= damage
+            console.log(this.name + "a attaqué " + pokemon.name + " pour " + damage + " dégat. Il lui reste " + pokemon.hp + "hp")
+        } else {
+            console.log(this.name + " a raté son attaque.")
+        }
+    }
+
+    isLucky() {
+        return Math.random() < this.luck
+    }
 }
 
-let pikachu = new pokemon ("Pikachu", 50, 100, 1000)
-let dracofeu = new pokemon ("Dracofeu", 150, 50, 1000)
+let pikachu = new pokemon("Pikachu", 12, 10, 200, 0.9)
+let lucario = new pokemon("Lucario", 20, 10, 500, 0.001)
 
-console.log("Le Pokémon s'apelle ", dracofeu.name, "Il possède ", dracofeu.attack, "d'attaque !", "et a", dracofeu.hp, "de points de vie !" , "et", dracofeu.defense, "de défense !")
+while(pikachu.hp >= 0 || lucario >= 0) {
+
+    pikachu.attackPokemon(lucario)
+    if(lucario.hp <= 0) {
+        console.log(lucario.name + " est mort.")
+        break
+    }
+
+    lucario.attackPokemon(pikachu)
+    if(pikachu.hp <= 0) {
+        console.log(pikachu.name + " est mort.")
+        break
+    }
+
+}
